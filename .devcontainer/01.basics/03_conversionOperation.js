b0d6ec3fead0815f8259d7704e6f6cc852f94882
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-05 13:32:39 +0000

    conversion

--- .devcontainer/01.basics/03_conversionOperation.js
+++ .devcontainer/01.basics/03_conversionOperation.js
@@ -1,7 +1,20 @@
-let score = true
+// let score = true
 
-console.log(typeof score);
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" = 33
+//"33abc" = NaN
+//true = 1; false=0
+//conversion
+// let isLoggedIn = 1 //ise convert kro boolean m to
+// let booleanIsisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsisLoggedIn);
+//1 = true; 0 = false m convert hoga
+// "" = false ; "hitesh" = true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
 
