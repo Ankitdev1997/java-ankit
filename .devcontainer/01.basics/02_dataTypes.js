ce1e3c54587b3f05cfe5226d08bee7b2976a0ea8
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-05 02:27:38 +0000

    data type class

---
+++ .devcontainer/01.basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" //treat all js code as newer version
+
+//alert( 3+3 ) we are using node js, not browser
+
+console.log(
+    3
+    +
+    3
+)//bad practice code must be readable
+console.log("hitesh");
+
