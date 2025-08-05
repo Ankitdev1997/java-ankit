616d73ff7115631c4f7a7f67eac455ab8d5ddba6
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-05 12:26:14 +0000

    datatype class

--- .devcontainer/01.basics/02_dataTypes.js
+++ .devcontainer/01.basics/02_dataTypes.js
@@ -8,4 +8,16 @@ console.log(
     3
 )//bad practice code must be readable
 console.log("hitesh");
+//types of data types
+//number=2 to power 53
+// bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined =
+//symbol = for uniqueness
 
+
+//objects
+// how to check datatype
+console.log(typeof "hitesh");
