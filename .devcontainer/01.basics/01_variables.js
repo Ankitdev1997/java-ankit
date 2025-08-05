cd722ef8394b2014401eb6e1dd9a6a77d9d31eaf
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-05 01:52:00 +0000

    variables

---
+++ .devcontainer/01.basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14415
+let accountEmail = "ankit2313@gmail.com"
+var accontPassword = "12345"
+accontCity = "jaipur"
+let accountState;
+// accountId = 2 not allowed to change const
+/* do not use (var) ever because of issue in functional scope and block scope*/
+accountEmail = "hc@akki.com"
+accontPassword = "21212121"
+accontCity = "bengaluru"
+console.log(accountId);
+console.table([accountId,accontPassword,accontCity,accountEmail,accountState])
+
+
