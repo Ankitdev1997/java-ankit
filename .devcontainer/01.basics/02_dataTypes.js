6e9f0b129e3d3d8955b1bc138da40f9ac7a53c7c
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-05 12:43:53 +0000

    datatype class

--- .devcontainer/01.basics/02_dataTypes.js
+++ .devcontainer/01.basics/02_dataTypes.js
@@ -8,6 +8,11 @@ console.log(
     3
 )//bad practice code must be readable
 console.log("hitesh");
+let name = "ankit"
+let age = 18
+
+let isLogIn = false
+let state;
 //types of data types
 //number=2 to power 53
 // bigint
@@ -21,3 +26,12 @@ console.log("hitesh");
 //objects
 // how to check datatype
 console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof isLogIn);
+console.log(typeof state);
+console.log(typeof null); //object (output)
+
+console.log(typeof undefined); //output bhi undefined ayega
+
+
+
