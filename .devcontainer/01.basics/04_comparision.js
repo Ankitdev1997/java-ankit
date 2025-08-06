a8fb04d7fe17257adff1a01f71ad6fd1f624230e
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-06 12:39:06 +0000

    comparision

---
+++ .devcontainer/01.basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+console.log(2<1);
+console.log("2" > "2");
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);  // dhyan rakhe jab compare kre to datatype same ho ex. number number ho ya string string ho
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //yha convert kar diya isne null ko number m
+
+
+// console.log(undefined >= 0);
+// console.log(undefined >= 0);
+// === but ye stricly check krta h ex.
+// console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
