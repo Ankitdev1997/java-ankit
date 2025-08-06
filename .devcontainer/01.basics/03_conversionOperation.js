20e9e9630db347e78c4e1242edaf5b977c472937
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-06 12:10:29 +0000

    conversion

--- .devcontainer/01.basics/03_conversionOperation.js
+++ .devcontainer/01.basics/03_conversionOperation.js
@@ -17,4 +17,42 @@
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
+console.log(typeof someNumber);
 
+
+
+//**********************Operations************** */
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 ki power 3
+// console.log(2/2);
+// console.log(2%3); // remainder wala
+// console.log(2**2);  //2ki power 2
+// //above all are basic math operation
+// let str1 = "hello"
+// let str2 = " hitesh" //spcace diya h hitesh taki space dikhe output m
+
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // agar string first h to sabko string m convert krega or agar pehla no. h to sabko no. traet krega
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+// console.log(+true); //sahi tarika nhi h code must be readable
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;//postfix operation (++ ko bolte h incremental)
+// ++gameCounter//prefix operator
+// console.log(gameCounter);//102 is liye kyoki do bar ho hya gamecounter
