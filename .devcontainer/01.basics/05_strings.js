09929a4dcf9abb810d40b4aafb0cf2e3f63eec11
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-07 12:58:14 +0000

    strings

---
+++ .devcontainer/01.basics/05_strings.js
@@ -0,0 +1,32 @@
+// const name = "ankit"
+// const repoCount = 50
+
+// //console.log(name + repoCount + " value"); outdated aajkal use nhi hota concatinate(combine) karne k liye ab ye use hota h 
+// console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+// // new method string likhne ka
+// const gameName = new String("hiteshhc")
+// const gameChange = "hiteshhc"
+// console.log(gameChange[0]);
+
+// console.log(gameName[0]);//[keyvalue = konse no. p konsa word h jase 0 no. p (H) h.]
+// console.log(gameName.__proto__);// matlab . do bar underscore ye h prototype jo ki iska string h
+// console.log(gameName.length);
+//strings k method
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));//index btane k liye
+// console.log(gameName.indexOf('t'));//kis position p t h.
+// const newString = gameName.substring(0,4)//last value 4(index) include nhi ho rhi h
+// console.log(newString);
+// const anotherString = gameName.slice(-4,-2)//isme -ve value bhi le sakte h
+// console.log(anotherString);
+// const newStringOne =  "    hitesh    "
+// console.log(newStringOne);//jab ye space nhi chaiye to
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'));//agar kuch replace krna h to
+// console.log(url.includes("hitesh"));//sawal puchne k liye h ki nnahi include
+const gameName= new String('hitesh-hc-com')
+
+console.log(gameName.split('-'));
