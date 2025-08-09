8a0aba129bb434f6d1a8fb3b3eac7b1b308e3cfb
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-09 04:23:10 +0000

    data

--- .devcontainer/01.basics/datatypes-summary.js
+++ .devcontainer/01.basics/datatypes-summary.js
@@ -9,10 +9,10 @@
 // const outsideTemp = null
 // let userEmail;
 
-// const id = Symbol('123')
-// const anotherId = Symbol('123')
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,7 +32,7 @@
 //     console.log("Hello world");
 // }
 
-// console.log(typeof anotherId);
+console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
@@ -45,18 +45,18 @@
 // there are two types of memory
 //stack(primitive m use hoti h)  jab bhi stack memmory use hogi to variable ki copy milegi
 // and heap(non-primitive m use hoti h) but jab heap use hogi to original value ka refrence milega
-let myYoutubename = "ankitprajapatdotcom"
-let anothername = myYoutubename
-anothername = "chaiaurcode"
-console.log(anothername);// isme hme copy di jati h to isme to change hoga but original m nhi
-console.log(myYoutubename);
-let userOne = {
-    email: "user@google.com",
-    upi:  "user@ybl"
-}
-let userTwo = userOne
-userTwo.email = "hitesh@google.com"
-console.log(userOne.email);
-console.log(userTwo.email);
+// let myYoutubename = "ankitprajapatdotcom"
+// let anothername = myYoutubename
+// anothername = "chaiaurcode"
+// console.log(anothername);// isme hme copy di jati h to isme to change hoga but original m nhi
+// console.log(myYoutubename);
+// let userOne = {
+//     email: "user@google.com",
+//     upi:  "user@ybl"
+// }
+// let userTwo = userOne
+// userTwo.email = "hitesh@google.com"
+// console.log(userOne.email);
+// console.log(userTwo.email);
 
 
