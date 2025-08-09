48d08dd60a7e1c1d64e78ef5895e5f547d6bdd1c
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-09 04:19:32 +0000

    comparision

--- .devcontainer/01.basics/04_comparision.js
+++ .devcontainer/01.basics/04_comparision.js
@@ -1,7 +1,7 @@
 // console.log(2>1);
 // console.log(2>=1);
-console.log(2<1);
-console.log("2" > "2");
+// console.log(2<1);
+// console.log("2" > "2");
 // console.log(2!=1);
 
 
@@ -13,10 +13,10 @@ console.log("2" > "2");
 // console.log(null >= 0); //yha convert kar diya isne null ko number m
 
 
-// console.log(undefined >= 0);
-// console.log(undefined >= 0);
+console.log(undefined >= 0);
+console.log(undefined >= 0);
 // === but ye stricly check krta h ex.
-// console.log("2" === 2);
+console.log("2" === 2);
 
 
 
