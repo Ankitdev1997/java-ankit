4dbd4b33f812eceb4e968a79f1ff0a6b9eb07466
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-09 05:09:57 +0000

    strins

--- .devcontainer/01.basics/05_strings.js
+++ .devcontainer/01.basics/05_strings.js
@@ -6,18 +6,18 @@
 
 // // new method string likhne ka
 // const gameName = new String("hiteshhc")
-// const gameChange = "hiteshhc"
-// console.log(gameChange[0]);
+const gameName = "hiteshhc"
 
-// console.log(gameName[0]);//[keyvalue = konse no. p konsa word h jase 0 no. p (H) h.]
-// console.log(gameName.__proto__);// matlab . do bar underscore ye h prototype jo ki iska string h
-// console.log(gameName.length);
-//strings k method
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2));//index btane k liye
-// console.log(gameName.indexOf('t'));//kis position p t h.
-// const newString = gameName.substring(0,4)//last value 4(index) include nhi ho rhi h
-// console.log(newString);
+
+console.log(gameName[0]);//[keyvalue = konse no. p konsa word h jase 0 no. p (H) h.]
+console.log(gameName.__proto__);// matlab . do bar underscore ye h prototype jo ki iska string h
+console.log(gameName.length);
+// // strings k method
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));//index btane k liye
+console.log(gameName.indexOf('t'));//kis position p t h.
+const newString = gameName.substring(0,4)//last value 4(index) include nhi ho rhi h
+console.log(newString);
 // const anotherString = gameName.slice(-4,-2)//isme -ve value bhi le sakte h
 // console.log(anotherString);
 // const newStringOne =  "    hitesh    "
@@ -27,6 +27,6 @@
 // const url = "https://hitesh.com/hitesh%20choudhary"
 // console.log(url.replace('%20','-'));//agar kuch replace krna h to
 // console.log(url.includes("hitesh"));//sawal puchne k liye h ki nnahi include
-const gameName= new String('hitesh-hc-com')
+// const gameName= new String('hitesh-hc-com')
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
