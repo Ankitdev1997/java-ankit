92747ab66d1e3f49795336e6b8aceff40bf4c6b3
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-13 17:42:35 +0000

    func

--- 03.basics/01_functions.js
+++ 03.basics/01_functions.js
@@ -36,10 +36,65 @@ const result = twoNumbers(3,5)
 
 
 
-function loginUserMessage(username){
+// function loginUserMessage(username){
+// //     if(username===undefined){console.log("please enter a user name")//both same   !sign dalne s true chiz false ho jati h aur false chiz true in if staement.
+// // return
+// //     }
+//     if(!username){console.log("please enter a user name")
+// return
+//     }
+// return `${username} just logged in`
+// }
+// // console.log( loginUserMessage("ankit"));
+// console.log(loginUserMessage());
+
+
+
+
+function loginUserMessage(username = "sam")//agar upar hi value de de to kabhi bhi undefine nhi ayegi
+{
+//     if(username===undefined){console.log("please enter a user name")//both same(niche vala bhi)   !sign dalne s true chiz false ho jati h aur false chiz true in if staement.
+// return
+//     }
+    if(!username){console.log("please enter a user name")
+return
+    }
 return `${username} just logged in`
 }
-console.log( loginUserMessage("ankit"));
+// console.log( loginUserMessage("ankit"));
+// console.log(loginUserMessage("hitesh"));
 
 
+
+function calculateCartPrice(val1, val2 , ...num1){                     //... matlab rest or spread operator as used case dono m se kuch bhi bol skte h
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500,600,700));// isme val 1 ho gyi 200, and val2 ho gyi 400 and baki sari ...num1 k array m .
+
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+function handleObject(anyobject){
+console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user) 
+//2nd method direct object bhi rakh sakte h
+handleObject(
+    {
+        username: "sam",
+        price: 199
+    }
+)
+
+const myNewArray = [200,400,600]
+function returnSecondValue(getArray){
+return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(
+    [200,400,600]
+));
 
