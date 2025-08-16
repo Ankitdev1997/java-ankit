86430e96a695d038e49ff2e5c2d416f3d76c1ac6
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-16 18:13:26 +0000

    var

--- .devcontainer/01.basics/01_variables.js
+++ .devcontainer/01.basics/01_variables.js
@@ -4,7 +4,7 @@ var accontPassword = "12345"
 accontCity = "jaipur"
 let accountState;
 // accountId = 2 not allowed to change const
-/* do not use (var) ever because of issue in functional scope and block scope*/
+/* do not use (var) ever because of issue in global scope and block scope*/
 accountEmail = "hc@akki.com"
 accontPassword = "21212121"
 accontCity = "bengaluru"
