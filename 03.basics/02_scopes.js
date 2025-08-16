90314cd0839ace1483400dfe145e7a0e495fd322
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-16 05:43:59 +0000

    scope

--- 03.basics/02_scopes.js
+++ 03.basics/02_scopes.js
@@ -9,7 +9,7 @@ if(true){
 let a = 10 
 const b = 20 
 
-console.log("inner: ",a); // andar print kra to value a = 10 kyoki block scope
+// console.log("inner: ",a); // andar print kra to value a = 10 kyoki block scope
 
 }
 
@@ -36,9 +36,22 @@ if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        console.log(username + website);
+        // console.log(username + website);
     }
     // console.log(website);
 }
 
 // console.log(username);
+
+//+++++++++++interesting++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num + 1          //1st method function declare karne ka     // yha console ko upar bhi run krenge to error nhi ayega
+}
+
+
+// addTwo(5)
+const addTwo = function(num){            //2nd method variable ke base p function declare karna both are same bas kuch   difference h   // lekin yha error de dega
+    return num + 2
+}
+
