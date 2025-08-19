0ab2e226ea912dae3954a261fcb8aa728ef90de0
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-19 05:55:39 +0000

    truthy

--- 04.controlFlow/truthy.js
+++ 04.controlFlow/truthy.js
@@ -1,11 +1,11 @@
-const userEmail = "ankit2pra"
-if(userEmail){
-    console.log("got user email");     // koi condition nhi de to string ko hi truthy value man lega
+// const userEmail = "ankit2pra"
+// if(userEmail){
+//     console.log("got user email");     // koi condition nhi de to string ko hi truthy value man lega
     
-} else{
-    console.log("dont have user email");
+// } else{
+//     console.log("dont have user email");
     
-}  // truthy and falsy value chaiye
+// }  // truthy and falsy value chaiye
 
 
 //falsy
@@ -17,13 +17,41 @@ if(userEmail){
 //  [], "0"(string k andar 0 to truthy), "false"(string k andar to truthy)," "(space bhi de diya string m to truthy),{},function(){}
 
 //empty array and object ko check karna
-const emptyArr = []
-if(emptyArr.length == 0){
-    console.log("that is empty array");
+// const emptyArr = []
+// if(emptyArr.length == 0){
+//     console.log("that is empty array");
     
-}
-const emptyObj = {}
-if(Object.keys(emptyObj).length == 0){
-    console.log("that is empty object");
+// }
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("that is empty object");
     
-}
+// }
+
+// false == 0 ,  0 == "" , false == ""
+// all above statement are true
+
+
+// nullish coalescing operator(??): null , undefined k liye use karte h
+
+let val1;
+// val1 = 5 ?? 10
+
+// val1 = null ?? 10
+
+
+// val1 = undefined ?? 15    // in sabka matlab h agar value aa jati h to value show kar do varna null ya undefined de do
+
+val1 = null ?? 15 ?? 10   //isme jo pahle value milegi vo assign kar dega
+
+
+console.log(val1);
+
+
+//terniary operator (?)
+
+//condition ? true : false    syntax ase form m hoga
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
