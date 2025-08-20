f06158cc1104fbd546b3ea468cb50d6a43d83ad7
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-20 13:16:30 +0000

    while loop

---
+++ 05.iteration/two.js
@@ -0,0 +1,32 @@
+// while (condition) {
+    
+// }    // basic syntax
+// //example
+
+// let index = 0
+// while (index <= 10) {
+    
+    
+//     console.log(`value of index is ${index}`);
+//     index = index + 2 // ye condition h jo bad m execute hoti h ise increment/ decrement kese bhii likho
+    
+    
+// }
+
+// let myArry = ["flash","batman","superman"]
+// let arr = 0
+// while (arr < myArry.length) {
+//     console.log(`value is ${myArry[arr]}`);
+//     arr = arr +1   
+    
+// }
+
+// do {
+    
+// } while (condition);     // basic syntax isme pehle do(kam) hoga phir condition check
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);  // ek 11 dikha dega kyoki pehle kam hota h bad m condition check krega
