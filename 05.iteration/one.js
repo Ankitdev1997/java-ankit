ea1d85f7891f6609fa7e07186db8a12ed729a3f4
ankit prajapat <ankit1997prajapat@gmail.com>
2025-08-25 11:05:04 +0000

    one

--- 05.iteration/one.js
+++ 05.iteration/one.js
@@ -15,9 +15,10 @@
 // for (let i = 0; i < 10; i++) {      
 //     const element = i;
 //     if (element == 5 )
+//     {
 //         console.log("5 is best number");      //loop k andar if else vali condition
         
-//      {
+     
         
 //     }
 //     console.log(element);
